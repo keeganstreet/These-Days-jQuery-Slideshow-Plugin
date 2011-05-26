71b48bf982906838f35b4da9749f769ca302928f
Keegan Street <keeganstreet@gmail.com>
2011-05-26 15:29:11 +0200

    Made the JSLint config stricter. Following strict whitespace

--- js/jquery.tdslideshow.1.0.js
+++ js/jquery.tdslideshow.1.0.js
@@ -3,12 +3,12 @@
  * @link http://playground.thesedays.com/tdslideshow/
  * @author Keegan Street
  */
-(function($) {
+(function ($) {
 
 	var init, gotoSlide, stop, doTransition, publicMethods, css3 = window.Modernizr && window.Modernizr.csstransitions;
 
 	// Initialise plugin
-	init = function(options) {
+	init = function (options) {
 		var defaults = {
 			timeout: 2000,
 			speed: 1000,
@@ -16,7 +16,7 @@
 			beforeTransition: null,
 			currentClass: "current"
 		};
-		return this.each(function() {
+		return this.each(function () {
 			var $el = $(this), data = $el.data("tdslideshow");
 			if (!data) {
 				data = {};
@@ -26,7 +26,7 @@
 				data.$children = $el.children();
 				data.currentIndex = 0;
 				data.$current = data.$children.eq(0).addClass(data.options.currentClass);
-				data.timeoutId = setTimeout(function() {
+				data.timeoutId = setTimeout(function () {
 					doTransition($el);
 				}, data.options.timeout);
 				// Hide all images except first
@@ -42,17 +42,19 @@
 	};
 
 	// Go directly to a slide
-	gotoSlide = function(index) {
-		return this.each(function() {
+	gotoSlide = function (index) {
+		return this.each(function () {
 			doTransition($(this), index);
 		});
 	};
 
 	// Stop the slideshow
-	stop = function() {
-		return this.each(function() {
+	stop = function () {
+		return this.each(function () {
 			var $el = $(this), data = $el.data("tdslideshow");
-			if (!data || !data.timeoutId) { return; }
+			if (!data || !data.timeoutId) {
+				return;
+			}
 			data.stopped = true;
 			clearTimeout(data.timeoutId);
 			$el.data("tdslideshow", data);
@@ -60,16 +62,23 @@
 	};
 
 	// Show the next slide
-	doTransition = function($el, nextIndex) {
+	doTransition = function ($el, nextIndex) {
 		var data = $el.data("tdslideshow"), $next, speed, animationComplete;
-		if (!data) { return; } // Exit because element is removed from DOM
-		if (data.animating) { return; } // Do not play two animations at the same time
+		if (!data) { // Exit because element is removed from DOM
+			return;
+		}
+		if (data.animating) { // Do not play two animations at the same time
+			return;
+		}
 		data.animating = true;
 		clearTimeout(data.timeoutId); // Stop any tdslideshow timers that are currently running on this element
 
 		// Get next item
 		if (typeof nextIndex === "number") {
-			if (data.currentIndex === nextIndex) { data.animating = false; return; } // Do not allow transitions from a slide to the same slide
+			if (data.currentIndex === nextIndex) { // Do not allow transitions from a slide to the same slide
+				data.animating = false;
+				return;
+			}
 			data.currentIndex = nextIndex;
 			speed = data.options.fastSpeed;
 			$el.addClass("fastSpeed");
@@ -78,27 +87,33 @@
 			speed = data.options.speed;
 			$el.removeClass("fastSpeed");
 		}
-		if (data.currentIndex >= data.$children.length) { data.currentIndex = 0; }
+		if (data.currentIndex >= data.$children.length) {
+			data.currentIndex = 0;
+		}
 		$next = data.$children.eq(data.currentIndex);
 
 		// Call optional callback before doing the animation
-		if (typeof data.options.beforeTransition === "function") { data.options.beforeTransition.apply(this, [data.currentIndex]); }
+		if (typeof data.options.beforeTransition === "function") {
+			data.options.beforeTransition.apply(this, [data.currentIndex]);
+		}
 
 		// Do animation
 		data.$current.removeClass(data.options.currentClass);
 		$next.addClass(data.options.currentClass);
 		if (css3) { // Use CSS3 transitions if available
-			$next.bind("transitionend oTransitionEnd webkitTransitionEnd", function() {
+			$next.bind("transitionend oTransitionEnd webkitTransitionEnd", function () {
 				$next.unbind("transitionend oTransitionEnd webkitTransitionEnd");
 				animationComplete();
 			});
 			$next.css({opacity: 1});
 		} else {
-			$next.fadeIn(speed, function() { animationComplete(); });
+			$next.fadeIn(speed, function () {
+				animationComplete();
+			});
 		}
 
 		// Callback for animation completion
-		animationComplete = function() {
+		animationComplete = function () {
 			if (css3) {
 				data.$current.css({'opacity': 0});
 			} else {
@@ -108,7 +123,7 @@
 			if (data.stopped) {
 				delete data.timeoutId;
 			} else {
-				data.timeoutId = setTimeout(function() {
+				data.timeoutId = setTimeout(function () {
 					doTransition($el);
 				}, data.options.timeout);
 			}
@@ -123,13 +138,13 @@
 		stop: stop
 	};
 
-	$.fn.tdslideshow = function(method) {
+	$.fn.tdslideshow = function (method) {
 		if (publicMethods[method]) {
 			return publicMethods[method].apply(this, Array.prototype.slice.call(arguments, 1));
-		} else if (typeof method === "object" || ! method) {
+		} else if (typeof method === "object" || !method) {
 			return publicMethods.init.apply(this, arguments);
 		} else {
-			$.error("Method "+method+" does not exist on jQuery.tdslideshow");
+			$.error("Method " + method + " does not exist on jQuery.tdslideshow");
 		}
 	};
 
