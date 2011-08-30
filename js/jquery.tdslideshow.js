5df96532b363b19da624aeb39aec26355f11bf9d
Keegan Street <keeganstreet@gmail.com>
2011-08-30 09:51:54 +0200

    Added Modernizr feature-detection so that it doesn't have to be added as a separate HTTP request

--- js/jquery.tdslideshow.js
+++ js/jquery.tdslideshow.js
@@ -1,11 +1,14 @@
 /**
- * These Days jQuery Slideshow Plugin v1.0.1
+ * These Days jQuery Slideshow Plugin v1.0.2
  * @link http://playground.thesedays.com/tdslideshow/
  * @author Keegan Street
  */
 (function ($) {
 
-	var init, gotoSlide, stop, doTransition, publicMethods, css3 = window.Modernizr && window.Modernizr.csstransitions;
+	var init, gotoSlide, stop, doTransition, publicMethods;
+
+	// If you have already included Modernizr with a test for CSS Transitions, you can delete this line
+	var Modernizr=function(a,b,c){function A(a,b){var c=a.charAt(0).toUpperCase()+a.substr(1),d=(a+" "+n.join(c+" ")+c).split(" ");return z(d,b)}function z(a,b){for(var d in a)if(k[a[d]]!==c)return b=="pfx"?a[d]:!0;return!1}function y(a,b){return!!~(""+a).indexOf(b)}function x(a,b){return typeof a===b}function w(a,b){return v(prefixes.join(a+";")+(b||""))}function v(a){k.cssText=a}var d="2.0.6",e={},f=!0,g=b.documentElement,h=b.head||b.getElementsByTagName("head")[0],i="modernizr",j=b.createElement(i),k=j.style,l,m=Object.prototype.toString,n="Webkit Moz O ms Khtml".split(" "),o={},p={},q={},r=[],s,t={}.hasOwnProperty,u;!x(t,c)&&!x(t.call,c)?u=function(a,b){return t.call(a,b)}:u=function(a,b){return b in a&&x(a.constructor.prototype[b],c)},o.csstransitions=function(){return A("transitionProperty")};for(var B in o)u(o,B)&&(s=B.toLowerCase(),e[s]=o[B](),r.push((e[s]?"":"no-")+s));v(""),j=l=null,e._version=d,e._domPrefixes=n,e.testProp=function(a){return z([a])},e.testAllProps=A,g.className=g.className.replace(/\bno-js\b/,"")+(f?" js "+r.join(" "):"");return e}(this,this.document);
 
 	// Initialise plugin
 	init = function (options) {
@@ -14,10 +17,10 @@
 			speed: 1000,
 			fastSpeed: 100,
 			beforeTransition: null,
-			currentClass: "current"
+			currentClass: 'current'
 		};
 		return this.each(function () {
-			var $el = $(this), data = $el.data("tdslideshow");
+			var $el = $(this), data = $el.data('tdslideshow');
 			if (!data) {
 				data = {};
 				data.animating = false;
@@ -30,13 +33,13 @@
 					doTransition($el);
 				}, data.options.timeout);
 				// Hide all images except first
-				if (css3) {
+				if (Modernizr.csstransitions) {
 					data.$current.siblings().css({'opacity': 0});
 				} else {
 					data.$current.siblings().hide();
 				}
 				// Attach the plugin data to the element
-				$el.data("tdslideshow", data);
+				$el.data('tdslideshow', data);
 			}
 		});
 	};
@@ -51,19 +54,19 @@
 	// Stop the slideshow
 	stop = function () {
 		return this.each(function () {
-			var $el = $(this), data = $el.data("tdslideshow");
+			var $el = $(this), data = $el.data('tdslideshow');
 			if (!data || !data.timeoutId) {
 				return;
 			}
 			data.stopped = true;
 			clearTimeout(data.timeoutId);
-			$el.data("tdslideshow", data);
+			$el.data('tdslideshow', data);
 		});
 	};
 
 	// Show the next slide
 	doTransition = function ($el, nextIndex) {
-		var data = $el.data("tdslideshow"), $next, speed, animationComplete;
+		var data = $el.data('tdslideshow'), $next, speed, animationComplete;
 		if (!data) { // Exit because element is removed from DOM
 			return;
 		}
@@ -73,18 +76,18 @@
 		data.animating = true;
 
 		// Get next item
-		if (typeof nextIndex === "number") {
+		if (typeof nextIndex === 'number') {
 			if (data.currentIndex === nextIndex) { // Do not allow transitions from a slide to the same slide
 				data.animating = false;
 				return;
 			}
 			data.currentIndex = nextIndex;
 			speed = data.options.fastSpeed;
-			$el.addClass("fastSpeed");
+			$el.addClass('fastSpeed');
 		} else {
 			data.currentIndex = data.$current.index() + 1;
 			speed = data.options.speed;
-			$el.removeClass("fastSpeed");
+			$el.removeClass('fastSpeed');
 		}
 		if (data.currentIndex >= data.$children.length) {
 			data.currentIndex = 0;
@@ -95,16 +98,16 @@
 		clearTimeout(data.timeoutId);
 
 		// Call optional callback before doing the animation
-		if (typeof data.options.beforeTransition === "function") {
+		if (typeof data.options.beforeTransition === 'function') {
 			data.options.beforeTransition.apply(this, [data.currentIndex]);
 		}
 
 		// Do animation
 		data.$current.removeClass(data.options.currentClass);
 		$next.addClass(data.options.currentClass);
-		if (css3) { // Use CSS3 transitions if available
-			$next.bind("transitionend oTransitionEnd webkitTransitionEnd", function () {
-				$next.unbind("transitionend oTransitionEnd webkitTransitionEnd");
+		if (Modernizr.csstransitions) { // Use CSS transitions if available
+			$next.bind('transitionend oTransitionEnd webkitTransitionEnd', function () {
+				$next.unbind('transitionend oTransitionEnd webkitTransitionEnd');
 				animationComplete();
 			});
 			$next.css({opacity: 1});
@@ -116,7 +119,7 @@
 
 		// Callback for animation completion
 		animationComplete = function () {
-			if (css3) {
+			if (Modernizr.csstransitions) {
 				data.$current.css({'opacity': 0});
 			} else {
 				data.$current.hide();
@@ -130,7 +133,7 @@
 				}, data.options.timeout);
 			}
 			data.animating = false;
-			$el.data("tdslideshow", data);
+			$el.data('tdslideshow', data);
 		};
 	};
 
@@ -143,10 +146,10 @@
 	$.fn.tdslideshow = function (method) {
 		if (publicMethods[method]) {
 			return publicMethods[method].apply(this, Array.prototype.slice.call(arguments, 1));
-		} else if (typeof method === "object" || !method) {
+		} else if (typeof method === 'object' || !method) {
 			return publicMethods.init.apply(this, arguments);
 		} else {
-			$.error("Method " + method + " does not exist on jQuery.tdslideshow");
+			$.error('Method ' + method + ' does not exist on jQuery.tdslideshow');
 		}
 	};
 
