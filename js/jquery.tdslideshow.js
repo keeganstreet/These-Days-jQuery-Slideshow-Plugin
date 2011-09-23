34f5363189107fbd7a9dc5603996b07732f0e7d4
Keegan Street <keeganstreet@gmail.com>
2011-09-23 14:15:51 +0200

    Added a start method

--- js/jquery.tdslideshow.js
+++ js/jquery.tdslideshow.js
@@ -5,7 +5,7 @@
  */
 (function ($) {
 
-	var init, gotoSlide, stop, doTransition, publicMethods;
+	var init, gotoSlide, stop, start, doTransition, publicMethods;
 
 	// If you have already included Modernizr with a test for CSS Transitions, you can delete this line
 	var Modernizr=function(a,b,c){function A(a,b){var c=a.charAt(0).toUpperCase()+a.substr(1),d=(a+" "+n.join(c+" ")+c).split(" ");return z(d,b)}function z(a,b){for(var d in a)if(k[a[d]]!==c)return b=="pfx"?a[d]:!0;return!1}function y(a,b){return!!~(""+a).indexOf(b)}function x(a,b){return typeof a===b}function w(a,b){return v(prefixes.join(a+";")+(b||""))}function v(a){k.cssText=a}var d="2.0.6",e={},f=!0,g=b.documentElement,h=b.head||b.getElementsByTagName("head")[0],i="modernizr",j=b.createElement(i),k=j.style,l,m=Object.prototype.toString,n="Webkit Moz O ms Khtml".split(" "),o={},p={},q={},r=[],s,t={}.hasOwnProperty,u;!x(t,c)&&!x(t.call,c)?u=function(a,b){return t.call(a,b)}:u=function(a,b){return b in a&&x(a.constructor.prototype[b],c)},o.csstransitions=function(){return A("transitionProperty")};for(var B in o)u(o,B)&&(s=B.toLowerCase(),e[s]=o[B](),r.push((e[s]?"":"no-")+s));v(""),j=l=null,e._version=d,e._domPrefixes=n,e.testProp=function(a){return z([a])},e.testAllProps=A,g.className=g.className.replace(/\bno-js\b/,"")+(f?" js "+r.join(" "):"");return e}(this,this.document);
@@ -60,6 +60,21 @@
 			}
 			data.stopped = true;
 			clearTimeout(data.timeoutId);
+			$el.data('tdslideshow', data);
+		});
+	};
+
+	// Restart the slideshow
+	start = function () {
+		return this.each(function () {
+			var $el = $(this), data = $el.data('tdslideshow');
+			if (!data || !data.stopped) {
+				return;
+			}
+			data.stopped = false;
+			data.timeoutId = setTimeout(function () {
+				doTransition($el);
+			}, data.options.timeout);
 			$el.data('tdslideshow', data);
 		});
 	};
@@ -140,7 +155,8 @@
 	publicMethods = {
 		init: init,
 		gotoSlide: gotoSlide,
-		stop: stop
+		stop: stop,
+		start: start
 	};
 
 	$.fn.tdslideshow = function (method) {
